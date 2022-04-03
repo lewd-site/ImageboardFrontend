82fefa6633375dbcd4b8681983782efe71de6ec7
Nina Lisitsinskaya <vvatashi@protonmail.com>
2022-04-03 11:55:56 +0600

    Update components, fix css minification

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,7 @@
 module.exports = {
-  plugins: [require('autoprefixer')],
+  plugins: [
+    require('postcss-sort-media-queries')({ sort: 'mobile-first' }),
+    require('autoprefixer'),
+    require('cssnano')({ preset: ['default', { discardComments: { removeAll: true } }] }),
+  ],
 };
